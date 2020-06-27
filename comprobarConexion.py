fb2444979ee4c0b2943cc14f8c414af079ec2a2a
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-06-27 14:55:42 -0500

    Doble Comprobacion

--- comprobarConexion.py
+++ comprobarConexion.py
@@ -2,6 +2,7 @@ import time
 import os
 from ctypes import windll, byref
 from ctypes.wintypes import DWORD
+from socket import gethostbyname, create_connection, error
 from playsound import playsound
 
 def comprobarConexion():
@@ -9,9 +10,20 @@ def comprobarConexion():
     conexion = windll.wininet.InternetGetConnectedState(byref(flags), None)
     return conexion
 
+def comprobarConexionAlterno():
+    try:
+        gethostbyname("google.com")
+        conexion = create_connection(("google.com", 80), 1)
+        conexion.close()
+        return True
+    except error:
+        return False
+
 while(True):
-    if not comprobarConexion():
+    if not (comprobarConexion() or comprobarConexionAlterno()):
+        print("No hay internet")
         playsound('Winding_Alarm_Clock.mp3')
     else:
+        print("Hay internet")
         playsound('Bugle_Tune.mp3')
     time.sleep(5)
